838054684bdd68d84227deb0efbab9394409c31d
Kang JooYoung <48624076+joo1948@users.noreply.github.com>
2019-10-25 00:53:36 +0900

    비밀번호 찾기 소스
    비밀번호 찾기 소스

---
+++ Find_pwd.java
@@ -0,0 +1,54 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.*;
+
+public class Find_pwd extends JFrame{
+
+	JLabel find_id, find_phone;
+	JTextField id_pwd, findphone;
+	JButton findpwdbtn;
+	
+	public Find_pwd() {
+		setTitle("PeachMelon 비밀번호 찾기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout()); 
+		c.setLayout(null);//배치관리자를  null로 고정시킴
+	
+		find_id = new JLabel("아이디 : ");
+		find_phone = new JLabel("전화번호 : ");
+		findphone = new JTextField("");
+		id_pwd = new JTextField("");
+		findpwdbtn = new JButton("확인");
+		
+		c.add(find_id);
+		c.add(id_pwd);
+		c.add(find_phone);
+		c.add(findphone);
+		c.add(findpwdbtn);
+		
+		
+		find_id.setBounds(600, 460, 200, 100);
+		id_pwd.setBounds(750,485,300,40);
+		find_phone.setBounds(565,535,200,100);
+		findphone.setBounds(750,565,300,40);
+		findpwdbtn.setBounds(1080,495,100,100);
+		
+		find_id.setFont(new Font("메이플스토리 OTF", Font.ITALIC, 35));
+		id_pwd.setFont(new Font("맑은고딕",Font.ITALIC,20));
+		find_phone.setFont(new Font("메이플스토리 OTF",Font.ITALIC,35));
+		findphone.setFont(new Font("맑은고딕",Font.ITALIC,20));
+		findpwdbtn.setFont(new Font("메이플스토리 OTF",Font.ITALIC,25));
+		
+		setBounds(80,50,1800,1000);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Find_pwd();
+	}
+
+}
