24439ab954799c09d39236183aa38f5fb77ba6b8
Kang JooYoung <48624076+joo1948@users.noreply.github.com>
2019-10-26 17:39:21 +0900

    회원가입
    회원가입 소스 배경 이미지 넣음

--- SignUp.java
+++ SignUp.java
@@ -4,28 +4,46 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class SignUp extends JFrame{
-
+	
 	private JLabel idL, passwdL, number;
 	private JTextField Idfield, Passwdfield,numberfield,mailfield;
 	private JButton checkbtn,createbtn;
 	private ImageIcon checkIcon, okayIcon;
 	
 	SignUp(){  
-	
+		 
+		
 		setTitle("PeachMelon 회원가입");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
 		c.setLayout(null);//배치관리자를  null로 고정시킴
 		 
-		 
+
 		idL= new JLabel("          ID : ");
 		passwdL = new JLabel("      Pwd : ");
 		number=new JLabel("    전화번호 :");
+		
+		//배경 이미지
+		ImageIcon display_sign = new ImageIcon("images/alldisplay.png");  
+		Image display_sign2= display_sign.getImage();
+				 
+		JPanel image = new JPanel() {//배경 넣기 
+			public void paintComponent(Graphics g) {
+				g.drawImage(display_sign2,0,0,getWidth(),getHeight(),this);
+				setOpaque(false);
+				super.paintComponent(g);
+			} 
+		};
+		
+		image.setLayout(null);
+		JScrollPane scrollPane = new JScrollPane(image);
+		setContentPane(scrollPane);
+		
 		//추가
-		c.add(number);
-		c.add(idL);
-		c.add(passwdL);
+		image.add(number);
+		image.add(idL);
+		image.add(passwdL);
 
 		
 		//아이디 비밀번호 위치 & 가로세로 조정
@@ -41,9 +59,9 @@ public class SignUp extends JFrame{
 		Passwdfield = new JTextField("");
 		numberfield=new JTextField("");
 		//추가
-		c.add(Idfield); 
-		c.add(Passwdfield);
-		c.add(numberfield); 
+		image.add(Idfield); 
+		image.add(Passwdfield);
+		image.add(numberfield); 
 		//아이디 비밀번호 적기의 위칭 & 가로세로 조정
 		Idfield.setBounds(690, 470, 400, 50);
 		Passwdfield.setBounds(690, 540, 400,50);
@@ -60,7 +78,7 @@ public class SignUp extends JFrame{
 		//중복체크버튼 폰트 정하기
 		checkbtn.setFont(new Font("메이플스토리 OTF", Font.ITALIC, 18)); 
 		//추가
-		c.add(checkbtn);
+		image.add(checkbtn);
 		 
 		//중복체크 팝업 다이얼로그
 		checkbtn.addActionListener(new ActionListener(){
@@ -68,7 +86,7 @@ public class SignUp extends JFrame{
 				//DB 내용 끌여들어와서 확인해줘야 함.
 				JOptionPane.showMessageDialog(null, "아이디가 이미 있습니다.","id 중복체크",JOptionPane.ERROR_MESSAGE);
 			}
-		}); 
+		});  
 		  
 		//오케이 이미지 아이콘
 		okayIcon = new ImageIcon("images/okay.png");
@@ -78,7 +96,7 @@ public class SignUp extends JFrame{
 		//생성버튼 위치 & 가로세로 조정
 		createbtn.setFont(new Font("메이플스토리 OTF", Font.ITALIC, 15));
 		//추가
-		c.add(createbtn);
+		image.add(createbtn);
 		 
 		//버튼의 외곽선을 없애준다. 
 		checkbtn.setBorderPainted(false);
@@ -94,6 +112,7 @@ public class SignUp extends JFrame{
 		setBounds(80,50,1800,1000);
 		setVisible(true);
 	}
+
 	public static void main(String[] args) {
 		new SignUp();
 	} 
