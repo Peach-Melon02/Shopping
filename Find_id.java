9ef9578be1df6f1b755b1c5dbd11625c0dd6b99a
Kang JooYoung <48624076+joo1948@users.noreply.github.com>
2019-10-26 17:37:52 +0900

    아이디 찾기 소스
    아이디 찾기 소스 배경이미지 넣음

--- Find_id.java
+++ Find_id.java
@@ -7,20 +7,39 @@ class Find_id extends JFrame{
 	JLabel findphone;
 	JTextField phonefield;
 	JButton phonebtn;
+	
 	Find_id(){ 
-		setTitle ("PeachMelon 아이디 찾기");
+		setTitle ("Book Shop 아이디 찾기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
 		c.setLayout(null);//배치관리자를  null로 고정시킴
 		  
+		
+		
 		findphone = new JLabel("전화번호 : ");
 		phonefield = new JTextField("");
-		phonebtn = new JButton("확인");
+		phonebtn = new JButton("확인"); 
 		
-		c.add(findphone); 
-		c.add(phonefield);
-		c.add(phonebtn); 
+		//배경 이미지
+		ImageIcon display_id = new ImageIcon("images/alldisplay.png");  
+		Image display_id2= display_id.getImage();
+		 
+		JPanel image = new JPanel() {//배경 넣기 
+			public void paintComponent(Graphics g) {
+				g.drawImage(display_id2,0,0,getWidth(),getHeight(),this);
+				setOpaque(false);
+				super.paintComponent(g);
+			} 
+		};
+		
+		image.setLayout(null);
+		JScrollPane scrollPane = new JScrollPane(image);
+		setContentPane(scrollPane);
+		
+		image.add(findphone); 
+		image.add(phonefield);
+		image.add(phonebtn); 
 		 
 		findphone.setFont(new Font("메이플스토리 OTF", Font.ITALIC, 35));
 		phonefield.setFont(new Font("맑은고딕", Font.ITALIC, 20));
@@ -38,7 +57,7 @@ class Find_id extends JFrame{
 				//DB 내용 끌여들어와서 확인해줘야 함.
 				JOptionPane.showMessageDialog(null, "아이디는 jooyoung1948 입니다","check id",JOptionPane.ERROR_MESSAGE);
 			}
-		}); 
+		});  
 		
 		setBounds(80,50,1800,1000);
 		setVisible(true);
