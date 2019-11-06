9516a96164d22f818a00b3da5d381d70be4d5d7a
Kang JooYoung <48624076+joo1948@users.noreply.github.com>
2019-11-06 21:13:44 +0900

    개발과목 디자인과목 일반과목
    배경 삽입만 함

--- Designdisplay.java
+++ Designdisplay.java
@@ -1,14 +1,85 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Designdisplay extends JFrame{
-	public Designdisplay() {
-		setTitle("Book shop_디자인과목");
+	public Designdisplay() { 
+		setTitle("book shop_디자인과목");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
-		c.setLayout(null); 
+		c.setLayout(null);  
+		 
+		JButton devbtn = new JButton(" ");//개발과
+		JButton designbtn = new JButton(" ");//디자인과
+		JButton basebtn = new JButton(" ");//인문과 
 		
+		
+		 
+		ImageIcon display = new ImageIcon("images/alldisplay.png"); //배경 이미지
+		Image display_1= display.getImage();
+		 
+		JPanel image = new JPanel() {//배경 넣기 
+			
+			public void paintComponent(Graphics g) {
+				g.drawImage(display_1,0,0,getWidth(),getHeight(),this);
+				setOpaque(false);
+				super.paintComponent(g);
+			} 
+		};
+		
+		image.add(devbtn);
+		image.add(designbtn);
+		image.add(basebtn);
+		
+		devbtn.setBounds(673,250,140,45);
+	    designbtn.setBounds(890,250,140,45);
+		basebtn.setBounds(1137,250,140,45);
+		
+		devbtn.setContentAreaFilled(false);
+		designbtn.setContentAreaFilled(false);
+		basebtn.setContentAreaFilled(false);
+		
+		devbtn.setFocusPainted(false);
+		designbtn.setFocusPainted(false);
+		basebtn.setFocusPainted(false);
+		
+		devbtn.setBorderPainted(false);
+		designbtn.setBorderPainted(false);
+		basebtn.setBorderPainted(false);
+		
+		devbtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				new Devdisplay();
+			}
+			 
+		});
+		designbtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new Designdisplay();
+			}
+			
+		});
+		basebtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new Basedisplay();
+			}
+			
+		});
+		
+		image.setLayout(null);
+		JScrollPane scrollPane=new JScrollPane(image);
+		setContentPane(scrollPane);
 		setBounds(80,50,1800,1000);
 		setVisible(true);
 	}
