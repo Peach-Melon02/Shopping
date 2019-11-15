0a003b6102ff8b782302c2bc83dbcebc52d145f8
Kang JooYoung <48624076+joo1948@users.noreply.github.com>
2019-11-15 23:50:33 +0900

    개발과목 이미지
    버튼들 조금만 삽입함
    images에서 이미지 사진의 크기는 180 X 230이다.

--- Devdisplay.java
+++ Devdisplay.java
@@ -4,20 +4,33 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class Devdisplay extends JFrame{
+public class Devdisplay extends JFrame implements ActionListener{
+	JButton devbtn, designbtn,basebtn;
+	private ImageIcon [] images= {
+			new ImageIcon("images/c.png"),
+			new ImageIcon("images/ita.png"),
+			new ImageIcon("images/computer.png"),
+			new ImageIcon("images/java.png"),
+			new ImageIcon("images/ds.png")};
+	
+	private JButton clang = new JButton(new ImageIcon("images/c.png"));
+	private JButton cplus = new JButton(new ImageIcon("images/cplus.png"));
+	private JButton computer = new JButton(new ImageIcon("images/computer.png"));
+	private JButton ita = new JButton(new ImageIcon("images/ita.png"));
+	private JButton javalang = new JButton(new ImageIcon("images/java.png"));
+	private JButton ds = new JButton(new ImageIcon("images/ds.png"));
+		
 	public Devdisplay() {
 		setTitle("Book shop_개발과목");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
-		c.setLayout(null); 
+		//c.setLayout(null); 
 		
-		JButton devbtn = new JButton(" ");//개발과
-		JButton designbtn = new JButton(" ");//디자인과
-		JButton basebtn = new JButton(" ");//인문과 
+		devbtn = new JButton(" ");//개발과
+		designbtn = new JButton(" ");//디자인과
+		basebtn = new JButton(" ");//인문과 
 		
-		
-		 
 		ImageIcon display = new ImageIcon("images/alldisplay.png"); //배경 이미지
 		Image display_1= display.getImage();
 		 
@@ -29,7 +42,11 @@ public class Devdisplay extends JFrame{
 				super.paintComponent(g);
 			} 
 		};
-		
+
+		image.setLayout(null);
+		JScrollPane scrollPane=new JScrollPane(image);
+		setContentPane(scrollPane);
+	
 		image.add(devbtn);
 		image.add(designbtn);
 		image.add(basebtn);
@@ -41,7 +58,7 @@ public class Devdisplay extends JFrame{
 		devbtn.setContentAreaFilled(false);
 		designbtn.setContentAreaFilled(false);
 		basebtn.setContentAreaFilled(false);
-		
+		 
 		devbtn.setFocusPainted(false);
 		designbtn.setFocusPainted(false);
 		basebtn.setFocusPainted(false);
@@ -50,39 +67,44 @@ public class Devdisplay extends JFrame{
 		designbtn.setBorderPainted(false);
 		basebtn.setBorderPainted(false);
 		
-		devbtn.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				new Devdisplay();
-			}
-			 
-		});
-		designbtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				new Designdisplay();
-			}
-			
-		});
-		basebtn.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				new Basedisplay();
-			}
-			
-		});
-	
+		devbtn.addActionListener(this); 
+		designbtn.addActionListener(this);
+		basebtn.addActionListener(this);
 		
 		
-		image.setLayout(null);
-		JScrollPane scrollPane=new JScrollPane(image);
-		setContentPane(scrollPane);
-		setBounds(80,50,1800,1000);
+		
+		
+		 
+		//여기서부터 이미지 클릭 문제임
+		image.add(clang);
+		clang.setBounds(130,370,170,233);
+		image.add(ita);
+		ita.setBounds(330,370,170,233);
+		image.add(javalang);
+		javalang.setBounds(530,370,170,233);
+		image.add(computer);
+		computer.setBounds(730,370,170,233);
+		
+		
+		
+		
+		
+		
+		 
+		 
+		
+		setBounds(80,20,1800,1000);
 		setVisible(true);
 	}
+	 
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==devbtn) new Devdisplay();
+		else if(e.getSource()==designbtn) new Designdisplay();
+		else if(e.getSource()==basebtn) new Basedisplay();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new Devdisplay();
+	}
+	
 }
