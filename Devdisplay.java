0d63ef854f6cc4e14f8e06b0732019c2dcf02d6b
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2019-11-16 16:39:08 +0900

    개발과목1
    다음페이지로 넘어가게 함.
    이미지 추가 계속 해야함

--- Devdisplay.java
+++ Devdisplay.java
@@ -6,32 +6,31 @@ import javax.swing.*;
 
 public class Devdisplay extends JFrame implements ActionListener{
 	JButton devbtn, designbtn,basebtn;
-	private ImageIcon [] images= {
-			new ImageIcon("images/c.png"),
-			new ImageIcon("images/ita.png"),
-			new ImageIcon("images/computer.png"),
-			new ImageIcon("images/java.png"),
-			new ImageIcon("images/ds.png")};
 	
-	private JButton clang = new JButton(new ImageIcon("images/c.png"));
-	private JButton cplus = new JButton(new ImageIcon("images/cplus.png"));
-	private JButton computer = new JButton(new ImageIcon("images/computer.png"));
-	private JButton ita = new JButton(new ImageIcon("images/ita.png"));
-	private JButton javalang = new JButton(new ImageIcon("images/java.png"));
-	private JButton ds = new JButton(new ImageIcon("images/ds.png"));
-		
+	private JButton clang = new JButton();
+	private JButton computer = new JButton();
+	private JButton ita = new JButton();
+	private JButton javalang = new JButton();
+	private JButton website = new JButton();
+	private JButton cg = new JButton();
+	private JButton cplus = new JButton();
+	private JButton database = new JButton();
+	private JButton ds = new JButton();
+	private JButton linux = new JButton();
+	private JButton next = new JButton();
+	
+	
 	public Devdisplay() {
-		setTitle("Book shop_개발과목");
+		setTitle("Book shop_개발과목"); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
-		//c.setLayout(null); 
 		
 		devbtn = new JButton(" ");//개발과
 		designbtn = new JButton(" ");//디자인과
 		basebtn = new JButton(" ");//인문과 
 		
-		ImageIcon display = new ImageIcon("images/alldisplay.png"); //배경 이미지
+		ImageIcon display = new ImageIcon("images/DEVimage1.jpg"); //배경 이미지
 		Image display_1= display.getImage();
 		 
 		JPanel image = new JPanel() {//배경 넣기 
@@ -76,22 +75,45 @@ public class Devdisplay extends JFrame implements ActionListener{
 		
 		 
 		//여기서부터 이미지 클릭 문제임
-		image.add(clang);
-		clang.setBounds(130,370,170,233);
-		image.add(ita);
-		ita.setBounds(330,370,170,233);
-		image.add(javalang);
-		javalang.setBounds(530,370,170,233);
+		image.add(clang); 
+		clang.setBounds(137,342,220,266);
+		clang.setContentAreaFilled(false); 
 		image.add(computer);
-		computer.setBounds(730,370,170,233);
+		computer.setBounds(447,342,220,266);//310
+		computer.setContentAreaFilled(false); 
+		image.add(ita);
+		ita.setBounds(757,342,220,266);
+		ita.setContentAreaFilled(false); 
+		image.add(javalang); 
+		javalang.setBounds(1067,342,220,266);
+		javalang.setContentAreaFilled(false); 
+		image.add(website);
+		website.setBounds(1377,342,220,266);
+		website.setContentAreaFilled(false); 
+		image.add(cg);
+		cg.setBounds(137,648,220,266);
+		cg.setContentAreaFilled(false); 
+		image.add(cplus);
+		cplus.setBounds(447,648,220,266);
+		cplus.setContentAreaFilled(false); 
+		image.add(database);
+		database.setBounds(757,648,220,266);
+		database.setContentAreaFilled(false); 
+		image.add(ds);
+		ds.setBounds(1067,648,220,266);
+		ds.setContentAreaFilled(false); 
+		image.add(linux);
+		linux.setBounds(1377,648,220,266);
+		linux.setContentAreaFilled(false); 
+		
+		image.add(next);
+		next.setBounds(1670,870,86,50);
+		next.setContentAreaFilled(false); 
+		next.setFocusPainted(false);
+		next.setBorderPainted(false);
+		next.addActionListener(this);
 		
 		
-		
-		
-		
-		
-		 
-		 
 		
 		setBounds(80,20,1800,1000);
 		setVisible(true);
@@ -101,10 +123,12 @@ public class Devdisplay extends JFrame implements ActionListener{
 		if(e.getSource()==devbtn) new Devdisplay();
 		else if(e.getSource()==designbtn) new Designdisplay();
 		else if(e.getSource()==basebtn) new Basedisplay();
+		else if(e.getSource()==next) new Devdisplay2();
 	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			new Devdisplay();
 	}
-	
 }
