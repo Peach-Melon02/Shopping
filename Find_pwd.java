558bf8ee2cfc72f61606fd894b3a68976ed5e6b5
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2019-11-25 19:28:50 +0900

    pw 찾기 코드
    DB 해결

--- Find_pwd.java
+++ Find_pwd.java
@@ -1,28 +1,48 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.*;
 
-public class Find_pwd extends JFrame{
+public class Find_pwd extends JFrame implements ActionListener{
 
 	JLabel find_id, find_phone;
 	JTextField id_pwd, findphone;
-	JButton findpwdbtn;
+	JButton findpwdbtn; 
 	
+	private PreparedStatement pstmt = null;
+	private Connection conn = null;
+	private ResultSet view = null;
+	 
 	public Find_pwd() { 
 		setTitle("PeachMelon 비밀번호 찾기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout()); 
 		c.setLayout(null);//배치관리자를  null로 고정시킴
-	
+		
+		try { 
+			Class.forName("org.gjt.mm.mysql.Driver");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping","root","mirim2");//jdbc에 연결하귀~~~~~~~~
+			//pstmt = conn.prepareStatement(sql); 
+		}catch(ClassNotFoundException e) {
+			handleError(e.getMessage());
+		}catch (SQLException e) {
+			handleError(e.getMessage());
+		}	
+	 
 		find_id = new JLabel("아이디 : ");
 		find_phone = new JLabel("전화번호 : ");
 		findphone = new JTextField("");
 		id_pwd = new JTextField("");
 		findpwdbtn = new JButton("확인");
 		
+		JButton BookShop = new JButton(" ");//bookshop으로 가는 페이지 
 		
 		//배경 이미지
 		ImageIcon display_pwd = new ImageIcon("images/alldisplay.png");
@@ -46,36 +66,78 @@ public class Find_pwd extends JFrame{
 		image.add(findphone);
 		image.add(findpwdbtn);
 		
+		image.add(BookShop);
+		BookShop.setBounds(450,20,1000,200);
+		BookShop.setContentAreaFilled(false);BookShop.setFocusPainted(false);//버튼의 테두리 없앰
+		BookShop.setBorderPainted(false);
+
+		BookShop.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) { 
+				new Home();
+				
+			}
+		});  
 		
 		find_id.setBounds(600, 460, 200, 100);
 		id_pwd.setBounds(750,485,300,40);
 		find_phone.setBounds(565,535,200,100);
 		findphone.setBounds(750,565,300,40);
-		findpwdbtn.setBounds(1080,495,100,100);
+		findpwdbtn.setBounds(1080,495,100,100); 
 		
 		find_id.setFont(new Font("메이플스토리 OTF", Font.ITALIC, 35));
 		id_pwd.setFont(new Font("맑은고딕",Font.ITALIC,20));
 		find_phone.setFont(new Font("메이플스토리 OTF",Font.ITALIC,35));
 		findphone.setFont(new Font("맑은고딕",Font.ITALIC,20));
 		findpwdbtn.setFont(new Font("메이플스토리 OTF",Font.ITALIC,25));
-		
 		//버튼의 내용을 투명하게 해준다
 		findpwdbtn.setContentAreaFilled(false);
+		findpwdbtn.addActionListener(this);
 		
-		findpwdbtn.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) { 
-				//DB 내용 끌여들어와서 확인해줘야 함.
-				JOptionPane.showMessageDialog(null, "jooyoung1948님의 \n비밀번호는 joo1948! 입니다.","check pwd",JOptionPane.ERROR_MESSAGE);
-			}
-		}); 
-		
-		setBounds(80,50,1800,1000); 
+		setBounds(80,20,1800,1000); 
 		setVisible(true);
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		new Find_pwd();
+	public void actionPerformed(ActionEvent e) { 
+		if(e.getSource()==findpwdbtn) {
+			String i_d = findphone.getText();
+			String p_w_d = id_pwd.getText();
+			FindPwd(i_d, p_w_d);
+		}
+		//DB 내용 끌여들어와서 확인해줘야 함.
+		
+	} 
+	private void FindPwd(String i_d, String p_w_d) {
+		try {
+			System.out.println("id :|"+i_d +"|");
+			System.out.println("pwd : |"+p_w_d+"|");
+			String sql = "select pw from info where tel =? and id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, i_d);
+			pstmt.setString(2, p_w_d);
+			view = pstmt.executeQuery();
+			
+			if(view.next()) {
+				
+				String find_pw=view.getString("pw");
+				
+				int result = JOptionPane.showConfirmDialog(null,"비밀번호는 "+find_pw+"입니다." , "pw 확인", JOptionPane.YES_NO_OPTION);
+				if(result==JOptionPane.OK_OPTION) {
+					findpwdbtn.setText("예");
+					findphone.setText(""); 
+					id_pwd.setText("");
+				}else {
+					findphone.setText("");
+					id_pwd.setText("");
+				}
+			}
+		}catch (SQLException e){
+			handleError(e.getMessage());
+		} 
 	}
 
+	private void handleError(String message) {
+		// TODO Auto-generated method stub
+		System.out.println("문제 : "+ message);
+		System.exit(1);
+	}
+	
 }
